878293e6b26435b70265dca778be174b9f1458fd
Ayush <ayushkansal303@gmail.com>
2024-12-15 22:13:03 +0530

    fixed user signin issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "@/lib/db";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
@@ -6,7 +7,33 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-    ]
+    ],
+    callbacks: {
+        async signIn(params) {
+            if (!params.user.email) {
+                return false
+            }
+            try {
+                const existingUser = await prismaClient.user.findUnique({
+                    where: {
+                        email: params.user.email
+                    }
+                });
+                if (!existingUser) {
+                    await prismaClient.user.create({
+                        data: {
+                            email: params.user.email,
+                            provider: 'GOOGLE'
+                        }
+                    });
+                }
+            } catch (error) {
+                console.error("Error during sign in:", error);
+                return false;
+            }
+            return true;
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
