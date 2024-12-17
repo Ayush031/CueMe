85d7e01acbf84337359d70b6713f83ed288ee654
Ayush <ayushkansal303@gmail.com>
2024-12-17 15:20:51 +0530

    fixed bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,5 +2,7 @@ import StreamView from "@/components/StreamView";
 import { creatorId } from "../lib/utils";
 
 export default function Dashboard() {
+
+  //@ts-expect-error ignore
   return <StreamView creatorId={creatorId} playVideo={true} />;
 }
